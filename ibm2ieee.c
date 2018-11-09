a52408a22c8bc99a00effe156dc4f1b3cbc74770
Mark Dickinson <mdickinson@enthought.com>
2018-11-09 19:40:13 +0000

    Add IBM64 to IEEE32 conversion.

--- ibm2ieee.c
+++ ibm2ieee.c
@@ -1,8 +1,18 @@
 #include "Python.h"
-#include "math.h"
 #include "numpy/ndarraytypes.h"
 #include "numpy/ufuncobject.h"
 #include "numpy/npy_3kcompat.h"
+
+
+static int bitlength32(npy_uint32 n)
+{
+  int n_bits = 0;
+  while (n) {
+    n >>= 1;
+    n_bits += 1;
+  }
+  return n_bits;
+}
 
 
 static PyMethodDef IBM2IEEEMethods[] = {
@@ -10,7 +20,7 @@ static PyMethodDef IBM2IEEEMethods[] = {
 };
 
 
-static void ibm2ieee32(char **args, npy_intp *dimensions,
+static void ibm32ieee32(char **args, npy_intp *dimensions,
                        npy_intp* steps, void* data)
 {
   npy_intp i;
@@ -18,7 +28,8 @@ static void ibm2ieee32(char **args, npy_intp *dimensions,
   char *in = args[0], *out = args[1];
   npy_intp in_step = steps[0], out_step = steps[1];
 
-  npy_uint32 tmp, significand, exponent, sign, result;
+  npy_uint32 tmp, significand, sign, result;
+  int exponent;
 
   for (i = 0; i < n; i++) {
     tmp = *(npy_uint32 *)in;
@@ -29,13 +40,12 @@ static void ibm2ieee32(char **args, npy_intp *dimensions,
 
     if (significand) {
       /* normalise */
-      while (significand < (npy_uint32)0x00800000U) {
-        significand <<= 1;
-        exponent -= 1U;
-      }
+      int shift = 24 - bitlength32(significand);
+      significand <<= shift;
+      exponent -= shift;
 
-      if ((npy_int32)exponent <= 0) {
-        if ((npy_int32)exponent < -23) {
+      if (exponent <= 0) {
+        if (exponent < -23) {
           /* underflow to zero */
           significand = 0;
           exponent = 0;
@@ -52,13 +62,13 @@ static void ibm2ieee32(char **args, npy_intp *dimensions,
           significand >>= 1;
           /* not possible for significand to be more than 2**23 at this point,
              due to limitations of IBM single precision */
-          exponent = 0U;
+          exponent = 0;
         }
       }
-      else if (exponent >= 255U) {
+      else if (exponent >= 255) {
         /* overflow to infinity */
         significand = 0U;
-        exponent = 255U;
+        exponent = 255;
       }
       else {
         significand -= 0x00800000U;
@@ -68,7 +78,7 @@ static void ibm2ieee32(char **args, npy_intp *dimensions,
       /* zero */
       exponent = 0;
     }
-    result = sign | (exponent << 23) | significand;
+    result = sign | ((npy_uint32)exponent << 23) | significand;
     *((npy_uint32 *)out) = result;
 
     in += in_step;
@@ -76,12 +86,101 @@ static void ibm2ieee32(char **args, npy_intp *dimensions,
   }
 }
 
-/*This a pointer to the above function*/
-PyUFuncGenericFunction funcs[1] = {&ibm2ieee32};
 
-static char types[2] = {NPY_UINT32, NPY_FLOAT32};
+static void ibm64ieee32(char **args, npy_intp *dimensions,
+                        npy_intp* steps, void* data)
+{
+  npy_intp i;
+  npy_intp n = dimensions[0];
+  char *in = args[0], *out = args[1];
+  npy_intp in_step = steps[0], out_step = steps[1];
 
-static void *data[1] = {NULL};
+  for (i = 0; i < n; i++) {
+    npy_uint64 tmp, significand;
+    npy_uint32 sign, significand_out, result;
+    int exponent;
+
+    tmp = *(npy_uint64 *)in;
+
+    significand = tmp & (npy_uint64)0x00FFFFFFFFFFFFFFU;
+    exponent = ((tmp & (npy_uint64)0x7F00000000000000U) >> 54) - 130;
+    sign = (tmp & (npy_uint64)0x8000000000000000U) >> 32;
+
+    if (significand) {
+      /* normalise */
+      while (significand < (npy_uint64)0x0080000000000000U) {
+        significand <<= 1;
+        exponent -= 1;
+      }
+
+      if (exponent <= 0) {
+        /* underflow */
+        if (exponent < -23) {
+          /* underflow to zero */
+          significand_out = 0U;
+          exponent = 0;
+        }
+        else {
+          /* underflow; apply round-ties-to-even */
+          npy_uint64 shift, mask, has_remainder, is_odd;
+          shift = 32 - exponent;
+          mask = ~((~(npy_uint64)0) << shift);
+          has_remainder = !!(significand & mask);
+          significand >>= shift;
+          is_odd = !!(significand & 2);
+          significand += (is_odd | has_remainder);
+          significand >>= 1;
+          significand_out = significand;
+          exponent = 0;
+        }
+      }
+      else if (exponent >= 255) {
+        /* overflow to infinity */
+        significand_out = 0U;
+        exponent = 255;
+      }
+      else {
+        npy_uint64 mask, has_remainder, is_odd;
+        significand -= 0x0080000000000000U;
+        /* Now have a 55-bit fraction; need a 23-bit fraction, so remove
+           32 bits, and round. */
+        mask = 0x7fffffffULL;
+        has_remainder = !!(significand & mask);
+        significand >>= 31;
+        is_odd = !!(significand & 2);
+        significand += (is_odd | has_remainder);
+        significand >>= 1;
+        significand_out = significand;
+      }
+    }
+    else {
+      /* zero */
+      significand_out = 0;
+      exponent = 0;
+    }
+    result = sign + ((npy_uint32)exponent << 23) + significand_out;
+
+    *((npy_uint32 *)out) = result;
+
+    in += in_step;
+    out += out_step;
+  }
+}
+
+PyUFuncGenericFunction funcs[2] = {
+  &ibm32ieee32,
+  &ibm64ieee32
+};
+
+static char types[4] = {
+  NPY_UINT32, NPY_FLOAT32,
+  NPY_UINT64, NPY_FLOAT32
+};
+
+static void *data[2] = {
+  NULL,
+  NULL
+};
 
 #if PY_VERSION_HEX >= 0x03000000
 static struct PyModuleDef moduledef = {
@@ -93,7 +192,7 @@ static struct PyModuleDef moduledef = {
   NULL,
   NULL,
   NULL,
-    NULL
+  NULL
 };
 
 PyMODINIT_FUNC PyInit_ibm2ieee(void)
@@ -107,7 +206,7 @@ PyMODINIT_FUNC PyInit_ibm2ieee(void)
   import_array();
   import_umath();
 
-  ibm2float32 = PyUFunc_FromFuncAndData(funcs, data, types, 1, 1, 1,
+  ibm2float32 = PyUFunc_FromFuncAndData(funcs, data, types, 2, 1, 1,
                                         PyUFunc_None, "ibm2float32",
                                         "docstring", 0);
 
--- test_ibm2ieee.py
+++ test_ibm2ieee.py
@@ -1,10 +1,34 @@
+from fractions import Fraction as F
 import unittest
 
 import numpy as np
 
 from npufunc_directory.ibm2ieee import ibm2float32
 
-test_pairs = [
+
+def from_ibm32(f):
+    """
+    Convert a bit-representation of an IBM single-precision hexadecimal
+    float to a fractions.Fraction instance.
+    """
+    if f != f & 0xffffffff:
+        raise ValueError("Input should be an unsigned 32-bit integer.")
+    sign = (f & 0x80000000) >> 31
+    exponent = ((f & 0x7f000000) >> 24) - 64
+    fraction = F(f & 0x00ffffff, 0x01000000)
+    result = fraction * F(16)**exponent
+    return -result if sign else result
+
+
+def to_ieee32(f):
+    """
+    Convert a fractions.Fraction instance to the closest representable
+    IEEE 754 binary32 value.
+    """
+
+
+
+single_to_single_pairs = [
     (0x00000000, 0.0),
     (0x1b100000, 0.0),
     (0x1b200000, 0.0),
@@ -13,6 +37,8 @@ test_pairs = [
     (0x1b800000, float.fromhex("0x1p-149")),
     (0x1bbfffff, float.fromhex("0x1p-149")),
     (0x1bc00000, float.fromhex("0x2p-149")),
+
+    (0x1fffffff, float.fromhex("0x1p-132")),
 
     (0x20fffff4, float.fromhex("0x0.fffff0p-128")),
 
@@ -50,9 +76,34 @@ test_pairs = [
 ]
 
 
+double_to_single_pairs = [
+    (0x1effffffffffffff, float.fromhex("0x1p-136")),
+    (0x1fffffffffffffff, float.fromhex("0x1p-132")),
+    (0x20ffffffffffffff, float.fromhex("0x1p-128")),
+
+    (0x213fffffbfffffff, float.fromhex("0x0.3fffff8p-124")),
+    (0x213fffffc0000000, float.fromhex("0x1p-126")),
+    (0x213fffffffffffff, float.fromhex("0x1p-126")),
+
+    (0x40ffffff7fffffff, float.fromhex("0x0.ffffffp+0")),
+    (0x40ffffff80000000, 1.0),
+    (0x411fffffefffffff, float.fromhex("0x0.ffffffp+1")),
+    (0x411ffffff0000000, 2.0),
+    (0x411fffffffffffff, 2.0),
+
+    (0x60ffffff7fffffff, float.fromhex("0x0.ffffffp+128")),
+    (0x60ffffff80000000, float("inf")),
+
+]
+
+double_to_single_pairs.extend(
+    (x * 2**32, f)
+    for x, f in single_to_single_pairs
+)
+
 class TestIBM2IEEE(unittest.TestCase):
-    def test_single_precision(self):
-        for input, expected in test_pairs:
+    def test_single_to_single(self):
+        for input, expected in single_to_single_pairs:
             pos_input = np.uint32(input)
             pos_expected = np.float32(expected)
             self.assertEqual(ibm2float32(pos_input), pos_expected)
@@ -61,6 +112,16 @@ class TestIBM2IEEE(unittest.TestCase):
             neg_expected = -np.float32(expected)
             self.assertEqual(ibm2float32(neg_input), neg_expected)
 
+    def test_double_to_single(self):
+        for input, expected in double_to_single_pairs:
+            pos_input = np.uint64(input)
+            pos_expected = np.float32(expected)
+
+            self.assertEqual(ibm2float32(pos_input), pos_expected)
+
+            neg_input = np.uint64(input + 0x8000000000000000)
+            neg_expected = -np.float32(expected)
+            self.assertEqual(ibm2float32(neg_input), neg_expected)
 
 
 
